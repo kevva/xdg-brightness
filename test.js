35b4db701e4ce64569572d82643dbf37267a3bff
Kevin Martensson <kevinmartensson@gmail.com>
2015-05-22 20:46:06 +0200

    Add tests

--- test.js
+++ test.js
@@ -3,6 +3,26 @@
 var test = require('ava');
 var xdgBrightness = require('./');
 
-test(function (t) {
-	t.end();
-});
+if (!process.env.CI) {
+	test('should get brightness', function (t) {
+		t.plan(2);
+
+		xdgBrightness.get(function (err, brightness) {
+			t.assert(!err, err);
+			t.assert(brightness);
+		});
+	});
+
+	test('should set brightness', function (t) {
+		t.plan(3);
+
+		xdgBrightness.set(0.5, function (err) {
+			t.assert(!err, err);
+
+			xdgBrightness.get(function (err, brightness) {
+				t.assert(!err, err);
+				t.assert(parseInt(brightness) === 0.5);
+			});
+		});
+	});
+}
